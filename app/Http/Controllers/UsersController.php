ea8c6c8879eb5f52abad5356d0ecf92952624b69
vagrant <vagrant@homestead>
2020-11-15 03:13:59 +0000

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,11 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth',['expect'=>['show']]);
+    }
     /**
      * Display a listing of the resource.
      *
@@ -60,6 +65,7 @@ class UsersController extends Controller
      */
     public function edit(User $user)
     {
+        $this->authorize('update',$user);
         return view('users.edit', compact('user'));
     }
 
@@ -73,6 +79,7 @@ class UsersController extends Controller
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
         //
+        $this->authorize('update', $user);
         $data = $request->all();
         if($request->avatar){
             $result = $uploader->save($request->avatar,'avatars',$user->id,416);
