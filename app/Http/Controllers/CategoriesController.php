b6223c8678df1eb83a96dba23a3e40e9e461a288
vagrant <vagrant@homestead>
2020-11-16 02:41:43 +0000

    话题排序

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -9,10 +9,10 @@ use App\Models\Topic;
 class CategoriesController extends Controller
 {
     //
-    public function show(Category $category)
+    public function show(Category $category,Request $request,Topic $topic)
     {
         # code...
-       $topics = Topic::where('category_id',$category->id)->paginate(20);
+       $topics = $topic->withOrder($request->order)->where('category_id',$category->id)->with('user','category')->paginate(20);
        return view('topics.index',compact('topics','category'));
     }
 }
