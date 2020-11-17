8fe7a741e42b030a73b5c425de983c6bfcd7832c
vagrant <vagrant@homestead>
2020-11-17 10:48:22 +0000

    用户可以删除自己发的话题

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -42,7 +42,9 @@
           <div class="topic-body mt-4 mb-4">
             {!! $topic->body !!}
           </div>
-
+          @can('update', $topic)
+            
+         
           <div class="operate">
             <hr>
             <a href="{{ route('topics.edit', $topic->id) }}" class="btn btn-outline-secondary btn-sm" role="button">
@@ -52,7 +54,7 @@
               <i class="far fa-trash-alt"></i> 删除
             </a>
           </div>
-
+        @endcan
         </div>
       </div>
     </div>
